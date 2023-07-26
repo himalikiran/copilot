5789dad683c4d90e28d331388b4a7d8f0266ccee
Himali <mybutwal@gmail.com>
2023-07-26 18:08:27 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,26 +1,34 @@
-// Create a Web Server
-// Run 'node comments.js' and open browser to http://localhost:3000/
-// See console.log for output
-// See browser for output
-
-// Load the http module to create an http server.
-var http = require('http');
-var querystring = require('querystring');
-var url = require('url');
-
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-  var pathname = url.parse(request.url).pathname;
-  var query = url.parse(request.url).query;
-  var name = querystring.parse(query)["name"];
-  var comment = querystring.parse(query)["comment"];
-  var email = querystring.parse(query)["email"];
-  var website = querystring.parse(query)["website"];
-  var output = "Name: " + name + "\nComment: " + comment + "\nEmail: " + email + "\nWebsite: " + website;
-  console.log("Request for " + pathname + " received.");
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write(output);
-  response.end();
+// Create Web Server
+// 1. Import express
+const express = require('express');
+// 2. Create an express object
+const app = express();
+// 3. Define a port
+const port = 3000;
+// 4. Create a web server
+app.listen(port, function() {
+    console.log('Server listening on port ' + port);
 });
 
-// Listen on port 3000, IP defaults to
+// Create a route for '/' and send 'Hello World' to the client
+app.get('/', function(req, res) {
+    res.send('Hello World');
+});
+
+// Create a route for '/comments' and send an array of comments to the client
+app.get('/comments', function(req, res) {
+    res.send(comments);
+});
+
+// Create a route for '/comments/:id' and send a single comment object to the client
+app.get('/comments/:id', function(req, res) {
+    res.send(comments[req.params.id]);
+});
+
+// Create an array of comment objects
+let comments = [
+    { body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac tellus vitae dolor lobortis ultricies. Nullam quis rutrum tellus, id egestas ipsum. Nulla facilisi. Quisque eget enim vitae urna tincidunt luctus. Nulla facilisi. Sed sit amet felis ut nisl vulputate tempus. Sed sed scelerisque dolor. Aenean nec faucibus quam, non tempus urna. Sed eget eros auctor, pellentesque nunc vel, facilisis velit. Sed at turpis quis orci pulvinar feugiat. Suspendisse potenti. Phasellus sed justo auctor, venenatis massa vel, ullamcorper risus. Nunc tempus nunc vitae fermentum porta. Morbi euismod, sem ac mattis ultricies, nunc nisl vulputate tellus, vitae egestas nunc nibh vitae nisl. Ut ac dolor euismod, pharetra enim quis, porta enim. Quisque sed mauris ut lacus ultricies pellentesque.',
+      username: 'anonymous',
+      postId: '1'
+    },
+    { body: 'Suspendisse potenti. Cras mollis, velit et aliquet lacinia, odio purus pulvinar nisi, sed aliquam leo augue ut ante. Donec sit amet eros sem. Ut at justo
